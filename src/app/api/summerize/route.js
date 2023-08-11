4a96eeba66d1a348a8d6eae23c37e7d4b5e35104
Roshan Hegde <roshanhegde@outlook.com>
2023-08-11 13:14:45 -0400

    user.txt removed and converted to string

--- src/app/api/summerize/route.js
+++ src/app/api/summerize/route.js
@@ -43,7 +43,23 @@ export async function POST(req) {
 
   console.log("User context:" + context);
 
-  const storyTemplate = await readFile("user.txt", "utf-8");
+  const storyTemplate = "h1: Title: Story # (use the story number) - <Story Title> \n" + 
+  "A user story is the smallest unit of work that needs to be done. A story (or user story) is a feature or requirement from the user’s perspective. Stories should be defined using non-technical language so anyone involved in the project can understand.\n" +
+  "h2: Purpose \n" +
+  "As a <Role> I want <Objective> so that <Motivation> \n" +
+  "h2: Description \n" +
+  "Provide application and user context to the developer and user to improve the understanding of the user story title. \n" +
+  "h2: Context \n" +
+  "Provide any context surrounding the reasons behind this story – key questions, user story, rationales, etc. \n" +
+  "h2: Technical Information \n" +
+  "Any technical information that provides context to this story (use your own knowledge for this) \n" +
+  "h2: Acceptance Criteria (use a <ul> element with <li> elements for each below) \n" +
+  "- Given precondition \n" +
+  "- When action \n" +
+  "- And applicable if additional actions are required \n" +
+  "- Then result \n" +
+  "(<b> tag)QA: (end tag or </b>) Tag the list of persons responsible for QA'ing this story \n"
+  
   const prompt =
     "Generate 10 user stories based on the project brief for the specificed customer within it. " +
     "The user stories should be returned as html, with a div (div should have a class name of content) surrounding all content and no newlines or anything else." +
